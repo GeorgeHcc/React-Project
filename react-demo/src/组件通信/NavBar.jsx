db18f433f581d5de2a49168d2d4d52680603d563
GeorgeH <default_hyn@163.com>
2023-02-20 17:16:03 +0800

    feat: add propTypes

--- react-demo/src/组件通信/NavBar.jsx
+++ react-demo/src/组件通信/NavBar.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-
+import propTypes from "prop-types";
 export class NavBar extends Component {
   render() {
-    const { banners } = this.props;
+    const { banners, title } = this.props;
     return (
       <ul>
         {banners.map((item) => (
-          <li>{item}</li>
+          <li title={title}>{item}</li>
         ))}
       </ul>
     );
   }
 }
+NavBar.propTypes = {
+  banners: propTypes.array.isRequired,
+};
+
+NavBar.defaultProps = {
+    title:'banners'
+};
